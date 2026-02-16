f3c4e700d2db60989332a0d1037402203d0c7098
Patrik848 <121981617+Patrik848@users.noreply.github.com>
2026-02-16 11:30:19 -0500

    updated routes/files

---
+++ src/pages/LanguageIntelligence/ContractDetail.tsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, FormEvent, ChangeEvent } from 'react';
+
+
+const ContractDetail: React.FC = () => {
+	return (
+		<div>
+		</div>
+	);
+}
+
+export default ContractDetail;
