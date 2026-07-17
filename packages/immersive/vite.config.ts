c964eb1c3248aea42a5e6b3c83498bfa167e1ad2
Shanley Verschueren <chainbot90@gmail.com>
2026-07-16 20:09:42 -0500

    fix: config files are fixed again after a mixup from rebase.

--- packages/immersive/vite.config.ts
+++ packages/immersive/vite.config.ts
@@ -11,4 +11,12 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    rollupOptions: {
+      external: [],
+    }
+  },
+  esbuild: {
+    tsconfig: path.resolve(__dirname, 'tsconfig.app.json'),
+  }
 })
